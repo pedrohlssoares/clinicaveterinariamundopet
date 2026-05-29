1779ae274d9b273ea7100f4b45392099ae1645ad
LuisSalvato <lcsalvato12011@gmail.com>
2026-05-29 19:54:24 -0300

    V2
    Modificações feitas para ter compatibilidade com o join

--- model/entity/consulta.php
+++ model/entity/consulta.php
@@ -7,14 +7,22 @@ class consulta{
     private $salaconsultafk;
     private $data;
     private $processos_feitos;
+    
+    private $nome_pet;
+    private $nome_veterinario;
+    private $numero_sala;
 
-    public function __construct($idconsulta, $petconsultafk, $veterinarioconsultafk, $salaconsultafk, $data, $processos_feitos){
+    public function __construct($idconsulta, $petconsultafk, $veterinarioconsultafk, $salaconsultafk, $data, $processos_feitos, $nome_pet = null, $nome_veterinario = null, $numero_sala = null){
         $this->idconsulta = $idconsulta;
         $this->petconsultafk = $petconsultafk;
         $this->veterinarioconsultafk = $veterinarioconsultafk;
         $this->salaconsultafk = $salaconsultafk;
         $this->data = $data;
         $this->processos_feitos = $processos_feitos;
+        
+        $this->nome_pet = $nome_pet;
+        $this->nome_veterinario = $nome_veterinario;
+        $this->numero_sala = $numero_sala;
     }
 
     public function __get($key){
--- model/entity/remedio.php
+++ model/entity/remedio.php
@@ -1,23 +1,31 @@
 <?php
 
-class remedio{
+class remedio {
     private $idremedio;
     private $produtoremediofk;
-    private $ativo; 
+    private $ativo;
     private $lote;
+    
+    private $nome;
+    private $preco;
+    private $quantidade;
 
-    public function __construct($idremedio, $produtoremediofk, $ativo, $lote){
+    public function __construct($idremedio, $produtoremediofk, $ativo, $lote, $nome = null, $preco = null, $quantidade = null) {
         $this->idremedio = $idremedio;
         $this->produtoremediofk = $produtoremediofk;
         $this->ativo = $ativo;
-        $this->lote = $lote; 
+        $this->lote = $lote;
+        
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->quantidade = $quantidade;
     }
 
-    public function __get($key){
+    public function __get($key) {
         return $this->{$key};
     }
 
-    public  function __set($key, $value){
+    public function __set($key, $value) {
         $this->{$key} = $value;
     }
 }
