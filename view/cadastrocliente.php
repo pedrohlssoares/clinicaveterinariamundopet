01afd8c0851ede33639e3dc2801a7e9187634d0b
Pedro H. Soares <pedrolacerda.alunoifsudestemg@gmail.com>
2026-05-31 16:54:23 -0300

    Update cadastrocliente.php

--- view/cadastrocliente.php
+++ view/cadastrocliente.php
@@ -6,84 +6,100 @@ $base = __DIR__ . '/../';
 include_once $base . "config/conexao.php";
 include_once $base . "model/entity/cliente.php";
 include_once $base . "model/dao/clientedao.php";
+include_once $base . "model/entity/endereco.php";
+include_once $base . "model/dao/enderecodao.php";
 
 include __DIR__ . "/topo.html";
 
 $cdao = new clientedao();
+$edao = new enderecodao();
 
+if (isset($_SESSION["resultado"])) {
+    $classe = $_SESSION["resultado"] ? "alert-success" : "alert-danger";
+    echo "<div class='alert {$classe}'>{$_SESSION["mensagem"]}</div>";
+    $_SESSION["resultado"] = null;
+    $_SESSION["mensagem"] = null;
+}
+
+if (isset($_GET["idcliente"])) {
+    $res_cli = $cdao->readId($_GET["idcliente"]);
+    $res_end = $edao->readId($res_cli["enderecoclientefk"]);
+} else {
+    $res_cli = ["idcliente" => "", "nome" => "", "cpf" => "", "email" => "", "celular" => "", "enderecoclientefk" => ""];
+    $res_end = ["idendereco" => "", "rua" => "", "cidade" => "", "bairro" => "", "numero" => "", "complemento" => ""];
+}
 ?>
 
-<?php
-    if (isset($_SESSION["resultado"])) {
-        if ($_SESSION["resultado"] == true) {
-            echo "<p>{$_SESSION["mensagem"]}</p>";
+<div class="container mt-4">
+    <h2 class="text-center">Cadastro de Cliente e Endereço</h2>
+    <form method="post" action="../controller/clientecontroller.php">
+        
+        <input type="hidden" name="idcliente" value="<?php echo $res_cli["idcliente"] ?>">
+        <input type="hidden" name="idendereco" value="<?php echo $res_end["idendereco"] ?>">
+
+        <div class="row justify-content-center">
+            <div class="col-md-5 border-end">
+                <h4>Dados Pessoais</h4>
+                <div class="p-1"><input type="text" class="form-control" name="nome" placeholder="Nome:" value="<?php echo $res_cli["nome"] ?>" required></div>
+                <div class="p-1"><input type="text" class="form-control" name="cpf" placeholder="CPF:" value="<?php echo $res_cli["cpf"] ?>" required></div>
+                <div class="p-1"><input type="email" class="form-control" name="email" placeholder="E-mail:" value="<?php echo $res_cli["email"] ?>"></div>
+                <div class="p-1"><input type="tel" class="form-control" name="celular" placeholder="Celular:" value="<?php echo $res_cli["celular"] ?>"></div>
+            </div>
+
+            <div class="col-md-5">
+                <h4>Endereço</h4>
+                <div class="p-1"><input type="text" class="form-control" name="rua" placeholder="Rua:" value="<?php echo $res_end["rua"] ?>"></div>
+                <div class="p-1"><input type="text" class="form-control" name="bairro" placeholder="Bairro:" value="<?php echo $res_end["bairro"] ?>"></div>
+                <div class="p-1"><input type="text" class="form-control" name="cidade" placeholder="Cidade:" value="<?php echo $res_end["cidade"] ?>"></div>
+                <div class="row p-1">
+                    <div class="col-4"><input type="text" class="form-control" name="numero" placeholder="Nº:" value="<?php echo $res_end["numero"] ?>"></div>
+                    <div class="col-8"><input type="text" class="form-control" name="complemento" placeholder="Comp.:" value="<?php echo $res_end["complemento"] ?>"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-10 offset-md-1 text-center mt-3">
+            <input type="submit" class="btn btn-primary w-50" value="Gravar Tudo" name="btGravar">
+        </div>
+    </form>
+</div>
+
+<hr>
+
+<table class="table table-hover mt-4">
+    <thead class="table-dark">
+        <tr>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>E-mail</th>
+            <th>Cidade</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $result = $cdao->read();
+        if (is_null($result) || empty($result)) {
+            echo "<tr><td colspan='5' class='text-center'>Nenhum cliente cadastrado.</td></tr>";
         } else {
-            echo "<p>Erro ao efetuar a operação.</p>";
+            foreach ($result as $item) {
+                $end_list = $edao->readId($item->enderecoclientefk);
+                $cidade = $end_list ? $end_list["cidade"] : "Não informado";
+
+                echo "<tr>";
+                echo "<td>{$item->nome}</td>";
+                echo "<td>{$item->cpf}</td>";
+                echo "<td>{$item->email}</td>";
+                echo "<td>{$cidade}</td>";
+                echo "<td>";
+                echo "<a href='index.php?idcliente={$item->idcliente}' class='me-2'><img src='img/alterar.png' width='18'></a>";
+                echo "<a href='../controller/clientecontroller.php?idcliente={$item->idcliente}' onclick='return confirm(\"Deseja excluir?\")'><img src='img/apagar.png' width='18'></a>";
+                echo "</td>";
+                echo "</tr>";
+            }
         }
-        $_SESSION["resultado"] = null;
-        $_SESSION["mensagem"] = null;
-    }
-
-    if (isset($_GET["idcliente"])) {
-        $result = $cdao->readId($_GET["idcliente"]);
-
-    } else {
-        $result = ["idcliente" => "", "nome" => "", "cpf" => "", "email" => "", "celular" => ""];
-    }
-?>
-
-<form method="post" action="controller/clientecontroller.php">
-
-    <input type="hidden" class="form-control" name="idcliente" value="<?php echo $result["idcliente"] ?>">
-    <div class="col-md-4 offset-md-4 p-1">
-        <input type="text" class="form-control" name="nome" placeholder="Digite o nome:" value="<?php echo $result["nome"] ?>">
-    </div>
-    <div class="col-md-4 offset-md-4 p-1">
-        <input type="text" class="form-control" name="cpf" placeholder="Digite o CPF:" value="<?php echo $result["cpf"] ?>">
-    </div>
-    <div class="col-md-4 offset-md-4 p-1">
-        <input type="email" class="form-control" name="email" placeholder="Digite o email:" value="<?php echo $result["email"] ?>">
-    </div>
-    <div class="col-md-4 offset-md-4 p-1">
-        <input type="tel" class="form-control" name="celular" placeholder="Digite o número de celular:" value="<?php echo $result["celular"] ?>">
-    </div>
-    <div class="col-md-4 offset-md-4 p-1">
-        <input type="submit" value="Gravar" name="btGravar">
-    </div>
-</form>
-
-<table class="table table-hover">
-    <tr>
-        <th>Nome</th>
-        <th>CPF</th>
-        <th>E-mail</th>
-        <th>Celular</th>
-    </tr>
-
-    <?php
-    $result = $cdao->read();
- 
-    if (is_null($result)) {
-        echo "<tr><td colspan='6'>Erro ao buscar os dados do banco</td></tr>";
-    } else {
-
-        foreach ($result as $item) {
-            echo "<tr>";
-            echo "<td>" . $item->nome . "</td>";
-            echo "<td>" . $item->cpf . "</td>";
-            echo "<td>" . $item->email . "</td>";
-            echo "<td>" . $item->celular . "</td>";
-            echo "<td>";
-            //link para alterar
-            echo "<a href='index.php?idcliente={$item->idcliente}'><img src='img/alterar.png' width='18'></a>";
-            //link para deletar
-            echo "<a href='controller/clientecontroller.php?idcliente={$item->idcliente}'><img src='img/apagar.png' width='18'></a>";
-            echo "</td>";
-            echo "</tr>";
-        }
-    }
-    ?>
+        ?>
+    </tbody>
 </table>
-<?php
-include __DIR__ . "/rodape.html";
-?>
+
+<?php include __DIR__ . "/rodape.html"; ?>
