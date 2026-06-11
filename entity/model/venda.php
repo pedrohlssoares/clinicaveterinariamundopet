59f8dacf8f17df9a6e555930f8556bfd875cd3d1
Pedro H. Soares <pedrolacerda.alunoifsudestemg@gmail.com>
2026-06-11 20:52:21 -0300

    Update venda.php

--- entity/model/venda.php
+++ entity/model/venda.php
@@ -1,14 +1,12 @@
 <?php
 class venda {
     private $idvenda;
-    private $pagamentovendafk;
     private $produtovendafk;
     private $quantidade;
     private $valor_unitario;
 
-    public function __construct($idvenda, $pagamentovendafk, $produtovendafk, $quantidade, $valor_unitario) {
+    public function __construct($idvenda, $produtovendafk, $quantidade, $valor_unitario) {
         $this->idvenda = $idvenda;
-        $this->pagamentovendafk = $pagamentovendafk;
         $this->produtovendafk = $produtovendafk;
         $this->quantidade = $quantidade;
         $this->valor_unitario = $valor_unitario;
